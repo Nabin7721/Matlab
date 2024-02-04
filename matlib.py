58009f7b82e0394a411fbc27cc046619b960e2e8
Nabin M <57736746+Nabin7721@users.noreply.github.com>
2024-02-04 14:55:15 -0500

    Update matlib.py

--- matlib.py
+++ matlib.py
@@ -4,7 +4,7 @@ verb1 = input("Mushroom: ")
 verb2 = input("feeling: ")
 famous_things = input("Ultimate_self: ")
 
-madlib = f"Computer science is {adj}! it help me earn my living, therefore the things that help me evolve is {verb1} \
+madlib = f"Computer science is very {adj}! it helps me earn my living, therefore the things that help me evolve is {verb1} \
 the reason is that i can feel {verb2}, so through the process I can meet {famous_things}!"
 
 print(madlib)
