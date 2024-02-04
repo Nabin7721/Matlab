bd79fe1ef9851be08a2040d66e69f308fd6c8104
Nabin M <57736746+Nabin7721@users.noreply.github.com>
2024-02-04 14:54:22 -0500

    Add files via upload

---
+++ matlib.py
@@ -0,0 +1,10 @@
+
+adj = input("Adjective: ")
+verb1 = input("Mushroom: ")
+verb2 = input("feeling: ")
+famous_things = input("Ultimate_self: ")
+
+madlib = f"Computer science is {adj}! it help me earn my living, therefore the things that help me evolve is {verb1} \
+the reason is that i can feel {verb2}, so through the process I can meet {famous_things}!"
+
+print(madlib)
